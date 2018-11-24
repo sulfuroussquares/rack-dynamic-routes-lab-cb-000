94332ed65c4de9a22230b44af5796f9ff6ff4058
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-11-24 17:56:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,7 @@ def call(env)
 
 if req.path.match(/items/)
   item_name = req.path.split("/items/").last
+  item = @@items.find{|x| x.name == item_name}
 else
 resp.write "Route not found"
 resp.status = 404
