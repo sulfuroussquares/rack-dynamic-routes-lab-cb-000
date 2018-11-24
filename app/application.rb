4de60545e8e090814a300dfbad0fe9ebcc36204e
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-11-24 17:59:21 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,9 @@ def call(env)
 
 if req.path.match(/items/)
   item_name = req.path.split("/items/").last
-  if @@items.find{|x| x.name == item_name}
+  if @@items.find{|x| x.name == item_name} == nil
+    resp.write "No such item"
+    resp.status = 400
   item = @@items.find{|x| x.name == item_name}
   resp.write item.price
 else
